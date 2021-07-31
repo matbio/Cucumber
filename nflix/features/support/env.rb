08e4e1fc2447b19843076f75f2fc040677cabb5c
Matheus Bio Salles <matheus.biosalles@gmail.com>
2021-07-31 02:08:02 -0300

    Correção nas notas e novos relatórios.

--- nflix/features/support/env.rb
+++ nflix/features/support/env.rb
@@ -23,5 +23,5 @@ end
 Capybara.configure do |config|
   config.default_driver = @driver
   config.app_host = CONFIG["url"]
-  config.default_max_wait_time = 5
+  config.default_max_wait_time = 20
 end
